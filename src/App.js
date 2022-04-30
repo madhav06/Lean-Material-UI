d25a7b921f0e8e3803d1c85a1b14efff94404b88
madhav06 <mnandan06@gmail.com>
2022-04-30 14:51:28 +0530

    Default themes in Material-UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Notes from './pages/Notes'
 import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+
+  }
+})
 
 function App() {
   return (
-    <Router>
+    <ThemeProvider theme={theme}>
+      <Router>
       <Switch>
         <Route exact path="/">
           <Notes />
@@ -14,6 +34,7 @@ function App() {
         </Route>
       </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
