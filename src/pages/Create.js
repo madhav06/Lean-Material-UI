cfeed773ee66282689a2cac89ff9a886273841dc
madhav06 <mnandan06@gmail.com>
2022-04-30 16:53:54 +0530

    Text Fields

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
@@ -6,8 +6,15 @@ import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
-const useStyles = makeStyles({})
+const useStyles = makeStyles({
+  field: {
+    marginBottom: 20,
+    marginTop: 20,
+    display: 'block'
+  }
+})
 
 {/*
 const useStyles = makeStyles({
@@ -27,10 +34,33 @@ const useStyles = makeStyles({
 
 export default function Create() {
   const classes = useStyles()
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+  const [titleError, setTitleError] = useState(false)
+  const [detailsError, setDetailsError] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTitleError(false)
+    setDetailsError(false)
+
+    if (title == ''){
+      setTitleError(true)
+    }
+
+    if (details == ''){
+      setDetailsError(true)
+    }
+
+    if(title && details) {
+      console.log(title, details)
+    }
+  }
 
   return (
     <Container>
       <Typography
+
         className={classes.title} 
         variant='h6'
         component='h2'
@@ -40,6 +70,42 @@ export default function Create() {
       >
         Create a new Note
       </Typography>
+
+      <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+        <TextField
+          className={classes.field}
+          onChange={(e) => setTitle(e.target.value)}
+          label='Note Title'
+          color='secondary'
+          variant='outlined'
+          fullWidth
+          required
+          error={titleError}
+        />
+
+        <TextField
+          className={classes.field}
+          onChange={(e) => setDetails(e.target.value)}
+          label='Details'
+          color='secondary'
+          variant='outlined'
+          fullWidth
+          required
+          multiline
+          rows={4}
+          error={detailsError}
+        />
+
+        <Button
+          className={classes.btn} 
+          type='submit' 
+          color='secondary' 
+          variant='contained'
+          endIcon={<KeyboardArrowRightIcon />}  
+        >
+          Submit
+        </Button>
+      </form>
 
       {/*button*
       <Button type='submit' color='primary' variant='contained' onClick={() => alert('Hello!')}>
@@ -54,7 +120,8 @@ export default function Create() {
       <AcUnitOutlinedIcon  color='error' fontSize='small' />
       <AcUnitOutlinedIcon  color='disabled' fontSize='small' />*/}
 
-      {/*button component with icons*/}
+      {/*button component with icons*
+
       <Button
         className={classes.btn} 
         type='submit' 
@@ -65,7 +132,7 @@ export default function Create() {
       >
         Submit
       </Button>
-      {/* startIcon, endIcon both are available.*/}
+      * startIcon, endIcon both are available.*/}
 
 
     </Container>
