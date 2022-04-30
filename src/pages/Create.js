b8c32cb69e8a9b8ab0911bdbf0ae6d2550a61ca2
madhav06 <mnandan06@gmail.com>
2022-04-30 13:26:27 +0530

    Using Icons in Material-UI

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
+import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+
 export default function Create() {
   return (
     <Container>
@@ -15,9 +19,31 @@ export default function Create() {
         Create a new Note
       </Typography>
 
+      {/*button*
       <Button type='submit' color='primary' variant='contained' onClick={() => alert('Hello!')}>
         Submit
+      </Button>/}
+
+      {/*icons*/}
+      {/*<br />
+      <AcUnitOutlinedIcon />
+      <AcUnitOutlinedIcon  color='secondary' fontSize='large' />
+      <AcUnitOutlinedIcon  color='action' fontSize='medium' />
+      <AcUnitOutlinedIcon  color='error' fontSize='small' />
+      <AcUnitOutlinedIcon  color='disabled' fontSize='small' />*/}
+
+      {/*button component with icons*/}
+      <Button 
+        type='submit' 
+        color='primary' 
+        variant='contained' 
+        onClick={() => alert('Hello! Welocme to Lean-Material-UI@madhav')}
+        endIcon={<KeyboardArrowRightIcon />}  
+      >
+        Submit
       </Button>
+      {/* startIcon, endIcon both are available.*/}
+
 
     </Container>
   )
