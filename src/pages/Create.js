0dedbc922357f251621c6e2ddef987a145bbd5d1
madhav06 <mnandan06@gmail.com>
2022-04-30 14:19:50 +0530

    Custom CSS, makeStyles Hook

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -5,11 +5,29 @@ import Container from '@material-ui/core/Container';
 import AcUnitOutlinedIcon from '@material-ui/icons/AcUnitOutlined';
 import SendIcon from '@material-ui/icons/Send';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  btn: {
+    fontSize: 60,
+    backgroundColor: 'violet',
+    '&:hover': {
+    backgroundColor: 'blue'
+    }
+  },
+  title: {
+    textDecoration: 'underline',
+    marginBottom: 20
+  }
+})
 
 export default function Create() {
+  const classes = useStyles()
+
   return (
     <Container>
-      <Typography 
+      <Typography
+        className={classes.title} 
         variant='h6'
         component='h2'
         gutterBottom
@@ -33,7 +51,8 @@ export default function Create() {
       <AcUnitOutlinedIcon  color='disabled' fontSize='small' />*/}
 
       {/*button component with icons*/}
-      <Button 
+      <Button
+        className={classes.btn} 
         type='submit' 
         color='primary' 
         variant='contained' 
